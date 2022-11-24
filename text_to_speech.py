aead37ea043704865bff8f3aecba765677364de1
Abhinav Sharma <118889813+Abhi111003@users.noreply.github.com>
2022-11-25 00:28:40 +0530

    Add files via upload

---
+++ text_to_speech.py
@@ -0,0 +1,19 @@
+
+from gtts import gTTS
+
+
+import os
+
+
+mytext = 'Welcome abhinav how are you ok nice great '
+
+language = 'en'
+
+
+myobj = gTTS(text=mytext, lang=language, slow=False)
+
+
+myobj.save("welcome.mp3")
+
+
+os.system("mpg321 welcome.mp3")
